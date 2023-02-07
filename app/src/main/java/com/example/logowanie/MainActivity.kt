54eff2af52a8afefbb53dcfcfe4ff44e734041ca
JacobSparrow <99054042+Jacob5parrow@users.noreply.github.com>
2023-02-07 19:59:18 +0100

    sprawdzanie czy pole nie jest puste

--- app/src/main/java/com/example/logowanie/MainActivity.kt
+++ app/src/main/java/com/example/logowanie/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.logowanie
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,16 +15,35 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.submit).setOnClickListener {
             val password1 = findViewById<EditText>(R.id.password1)
             val password2 = findViewById<EditText>(R.id.password2)
-            if (password1.text.toString() == password2.text.toString()) {
+            var x = 0
+            var y = 0
 
-                findViewById<TextView>(R.id.compatible).text = "zgodne"
+            if (password1.text.toString().isEmpty())
+            {
+                findViewById<EditText>(R.id.password1).setHint("pole nie może być puste")
+                x = 1
             }
-            if (password1.text.toString() != password2.text.toString()) {
-                findViewById<TextView>(R.id.compatible).text = "nie zgodne"
+            if (password2.text.toString().isEmpty())
+            {
+                findViewById<EditText>(R.id.password2).setHint("pole nie może być puste")
+                y = 1
+            }
+            if (x == 1 && y == 1)
+            {
+
             }
 
         }
+
+
+
     }
 }
+
+
+
+
+
+
 
 
