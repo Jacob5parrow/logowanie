ffc8603f900fcc3d2db0f0077ab19de804a33163
JacobSparrow <99054042+Jacob5parrow@users.noreply.github.com>
2023-02-07 21:35:39 +0100

    powinno działać ale nie działa

--- app/src/main/java/com/example/logowanie/MainActivity.kt
+++ app/src/main/java/com/example/logowanie/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.logowanie
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,23 +17,79 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.submit).setOnClickListener {
             val password1 = findViewById<EditText>(R.id.password1)
             val password2 = findViewById<EditText>(R.id.password2)
-            var x = 0
-            var y = 0
+            var password = ""
+
+            findViewById<EditText>(R.id.password1).hint = ""
+            findViewById<EditText>(R.id.password2).hint = ""
+
+            findViewById<TextView>(R.id.check).setTextColor(Color.BLACK)
+            findViewById<TextView>(R.id.upperLetter).setTextColor(Color.BLACK)
+            findViewById<TextView>(R.id.lowerLetter).setTextColor(Color.BLACK)
+            findViewById<TextView>(R.id.number).setTextColor(Color.BLACK)
+            findViewById<TextView>(R.id.special).setTextColor(Color.GREEN)
+
 
             if (password1.text.toString().isEmpty())
             {
-                findViewById<EditText>(R.id.password1).setHint("pole nie może być puste")
-                x = 1
-            }
-            if (password2.text.toString().isEmpty())
-            {
-                findViewById<EditText>(R.id.password2).setHint("pole nie może być puste")
-                y = 1
-            }
-            if (x == 1 && y == 1)
-            {
+                findViewById<EditText>(R.id.password1).hint = "pole nie może być puste"
 
             }
+
+            if (password2.text.toString().isEmpty())
+            {
+                findViewById<EditText>(R.id.password2).hint = "pole nie może być puste"
+            }
+
+            else
+            {
+
+                if (password1 == password2)
+                {
+                    findViewById<TextView>(R.id.check).setTextColor(Color.GREEN)
+                    password = password1.toString()
+                }
+                if (password1 != password2)
+                {
+                    findViewById<TextView>(R.id.check).setTextColor(Color.RED)
+                    password = password1.toString()
+
+                }
+
+                val wielka_litera = Regex(".*[A-Z].*")
+
+                if (password.matches(wielka_litera))
+                {
+                    findViewById<TextView>(R.id.upperLetter).setTextColor(Color.GREEN)
+                }
+
+                val mala_litera = Regex(".*[a-z].*")
+
+                if (password.matches(mala_litera))
+                {
+                    findViewById<TextView>(R.id.lowerLetter).setTextColor(Color.GREEN)
+                }
+
+                val liczba = Regex(".*[0-9].*")
+
+                if (password.matches(liczba))
+                {
+                    findViewById<TextView>(R.id.number).setTextColor(Color.GREEN)
+                }
+
+                val specialChars = "!@#$%^&*()_+-=?"
+
+                if (password.any { specialChars.contains(it) })
+                {
+                    findViewById<TextView>(R.id.special).setTextColor(Color.GREEN)
+                }
+
+
+
+
+
+
+            }
+
 
         }
 
